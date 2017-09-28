1523bfe84dd4d5176ccdaa3e536f082c8869e398
Chris Jones <zorlanv@gmail.com>
2017-09-29 00:02:19 +1000

    resize handles, reworked events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,31 +17,61 @@ export class AppComponent {
     if (!this.date) this.date = new Date();
     this.events = [
       {
+        id: 1,
         title: "Gerard's Birthday",
         startDate : new Date(2019,10,27,8,0,0),
-        endDate : new Date(2019,10,27,10,0,0),
+        allDay: true,
         colour: 'red'
       },
       {
-        title: "Lunch Date",
+        id: 2,
+        title: 'Lunch Date',
         startDate : new Date(2019,10,24,12,0,0),
         endDate : new Date(2019,10,24,13,30,0),
         colour: 'orange'
       },
       {
-        title: "Appointment",
+        id: 3,
+        title: 'Appointment',
         startDate : new Date(2019,10,26,16,30,0),
-        endDate : new Date(2019,10,26,18,0,0),
+        endDate : new Date(2019,10,26,18,0,0)
+      },
+      {
+        id: 4,
+        title: 'A',
+        startDate : new Date(2019,10,24,6,0,0),
+        endDate : new Date(2019,10,24,7,0,0),
+        colour: 'green'
+      },
+      {
+        id: 5,
+        title: 'B',
+        startDate : new Date(2019,10,25,7,0,0),
+        endDate : new Date(2019,10,25,9,0,0),
+        colour: 'purple'
+      },
+      {
+        id: 6,
+        title: 'C',
+        startDate : new Date(2019,10,26,9,0,0),
+        endDate : new Date(2019,10,26,12,0,0),
+        colour: 'pink'
+      },
+      {
+        id: 7,
+        title: 'D',
+        startDate : new Date(2019,10,28,6,0,0),
+        endDate : new Date(2019,10,28,18,0,0),
+        colour: 'teal'
       }
     ]
   }
 
-  incrementDate():void {
-    this.date.setDate(this.date.getDate()+1);
+  allowDrop = (event:ScheduledEvent, date:number, hour:number) => {
+    return (date%2==1 && hour%2==1);
   }
 
-  decrementDate():void {
-    this.date.setDate(this.date.getDate()-1);
+  removeEvent = ({event}:any) => {
+    this.events = this.events.filter(e => e.id != event.id);
   }
-
 }
--- src/app/scheduler/scheduler.component.ts
+++ src/app/scheduler/scheduler.component.ts
@@ -1,14 +1,16 @@
-import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { Component, Input, Output, OnInit, OnChanges, SimpleChange, EventEmitter } from '@angular/core';
 const ComponentStyle = require('!raw-loader!less-loader!./scheduler.component.less');
 
-export class ScheduledEvent {
-  startDate: Date;
-  endDate: Date;
+export interface ScheduledEvent {
   title: string;
+  startDate: Date;
+  endDate?: Date;
+  allDay?: boolean;
   colour?: string;
+  [x: string]: any;
 }
 
-export class DayOfWeek {
+class DayOfWeek {
   number: number;
   label: string;
 }
@@ -26,30 +28,37 @@ const DayPrefix = (day:number) => {
   templateUrl: './scheduler.component.html',
   styleUrls: [ ComponentStyle ]
 })
-export class SchedulerComponent implements OnInit {
+export class SchedulerComponent implements OnInit, OnChanges {
   // params
-  @Input()
-  events: ScheduledEvent[];
-  @Input()
-  startDate: Date;
-  startHour: number;
-  endHour: number;
-  showAllDay: boolean = true;
-  defaultEventColour: string = 'blue';
-  @Input()
-  disabled: boolean = false;
-  @Output()
-  onSlotClick = new EventEmitter();
+  @Input() events: ScheduledEvent[];
+  @Input() startDate: Date;
+  @Input() startHour: number = 6;
+  @Input() endHour: number = 18;
+  @Input() showAllDay: boolean = true;
+  @Input() defaultEventColour: string = 'blue';
+  @Input() allowDropFunction: (event:ScheduledEvent, date:number, hour:number, fullDate:Date) => boolean;
+  @Input() disabled: boolean = false;
+  @Output() onSlotClick = new EventEmitter();
+  @Output() onSlotDoubleClick = new EventEmitter();
+  @Output() onEventClick = new EventEmitter();
+  @Output() onEventRemove = new EventEmitter();
+  @Output() onEventDragStart = new EventEmitter();
+  @Output() onEventDragHover = new EventEmitter();
+  @Output() onEventDragEnd = new EventEmitter();
   // private members
-  hours: number[] = [6,7,8,9,10,11,12,13,14,15,16,17,18];
+  hours: number[] = [];
   days: DayOfWeek[] = [];
   eventsLookup: any = {};
   dragEvent: ScheduledEvent;
   dragDate: Date;
+  removeButtons: boolean = false;
 
   ngOnInit() {
     if (!this.startDate) this.startDate = new Date();
     else if (typeof(this.startDate)==='string') this.startDate = new Date(this.startDate);
+    for(let i=this.startHour;i<=this.endHour;i++){
+      this.hours.push(i);
+    }
     let dayDate = new Date(this.startDate.getTime());
     for(let i=0;i<7;i++) {
       if (i>0) dayDate.setDate(dayDate.getDate()+1);
@@ -59,52 +68,68 @@ export class SchedulerComponent implements OnInit {
       this.days.push({number:date, label:`${day} ${date}${prefix}`});
     }
     this.events.forEach(event => {
-      let key = event.startDate.getDate()+'_'+event.startDate.getHours(),
-      duration = (event.endDate.getTime() - event.startDate.getTime())/1000/60/60,
-      height = duration*2*100+Math.pow(duration*2, 2.5)+'%',
-      entry = Object.assign({}, event, {duration, height});
+      let key = event.startDate.getDate() + '_' + (event.allDay ? 'allday' : event.startDate.getHours()),
+      duration = event.allDay ? 0 : (event.endDate.getTime()-event.startDate.getTime())/1000/60/60,
+      entry = Object.assign({}, event, {duration});
       if (!this.eventsLookup[key]) this.eventsLookup[key] = [entry];
       else this.eventsLookup[key].push(entry);
     });
+    setTimeout(()=>{this.removeButtons = true},1);
   }
 
-  getSlotDate(date:number, time:number) {
+  ngOnChanges(changes:{ [x: string]: SimpleChange }) {
+    console.log(this.events);
+    //this.events = changes['events'].currentValue;
+  }
+
+  getSlotDate(date:number, hour?:number) {
     let slotDate = new Date(this.startDate.getTime());
     slotDate.setDate(date);
-    slotDate.setHours(Math.floor(time), time%1*60);
+    if (typeof(hour)!=='undefined') slotDate.setHours(Math.floor(hour), hour%1*60);
     return slotDate;
   }
 
-  slotClick(date:number, time:number) {
-    let slotDate = this.getSlotDate(date, time);
-    console.log("Slot Click", slotDate);
+  slotClick(date:number, hour?:number) {
+    let fullDate = this.getSlotDate(date, hour);
+    this.onSlotClick.emit({date,hour,fullDate, originalEvent:event});
+  }
+
+  slotDoubleClick(date:number, hour?:number) {
+    let fullDate = this.getSlotDate(date, hour);
+    this.onSlotDoubleClick.emit({date,hour,fullDate, originalEvent:event});
   }
 
   eventClick(event:any, scheduledEvent:ScheduledEvent) {
     event.stopPropagation();
-    console.log("Event Click", scheduledEvent);
+    this.onEventClick.emit({event: scheduledEvent, originalEvent:event});
+  }
+
+  eventRemoveClick(event:any, scheduledEvent:ScheduledEvent) {
+    event.stopPropagation();
+    this.onEventRemove.emit({event: scheduledEvent, originalEvent:event});
   }
 
   eventDragStart(event:any, scheduledEvent:ScheduledEvent) {
-    event.stopPropagation();
     this.dragEvent = scheduledEvent;
-    console.log("Drag Start", scheduledEvent);
+    this.onEventDragStart.emit({event: scheduledEvent, originalEvent:event});
   }
 
-  eventDragEnd(event:any, scheduledEvent:ScheduledEvent) {
-    event.stopPropagation();
-    this.dragEvent = null;
-    console.log("Drag End", scheduledEvent, this.dragDate);
-  }
-
-  eventDrop(event: any) {
-    console.log("Event Drop", this.dragEvent, this.dragDate);
-  }
-
-  allowDrop(event: any, date:number, time:number) {
+  eventDrop(event: any, date:number, hour?:number) {
     event.preventDefault();
-    let slotDate = this.getSlotDate(date, time);
-    this.dragDate = slotDate;
-    return false;
+    let fullDate = this.getSlotDate(date, hour);
+    this.dragDate = fullDate;
+    this.onEventDragEnd.emit({event: this.dragEvent, date, hour, fullDate, originalEvent:event});
+    this.dragEvent = null;
+  }
+
+  allowDrop(event: any, date:number, hour?:number) {
+    // TODO: don't call this on every drag, only when date/hour change
+    let fullDate = this.getSlotDate(date, hour);
+    if (!this.allowDropFunction || this.allowDropFunction(this.dragEvent, date, hour, fullDate)) {
+      event.preventDefault();
+      return false;
+    } else {
+      return true;
+    }
   }
 }
